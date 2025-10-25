02def4776a8e77b4acb7bcdbeb98b9475e9838c1
dominikgubala <dominik.gubala@gmail.com>
2025-10-25 17:29:45 +0200

    addition

--- src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
+++ src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
     }
 
     public double addition(double num1, double num2) {
-        return 0;
+        return num1 + num2;
     }
 
     public double subtraction(double num1, double num2) {

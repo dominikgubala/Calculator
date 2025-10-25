7ae7b272c0f64da2a85d8a6f525bf03ffabfd1ab
dominikgubala <dominik.gubala@gmail.com>
2025-10-25 17:25:42 +0200

    unit tests - red stage

--- src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
+++ src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
@@ -5,4 +5,20 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Zad 1 - Tworzenie kalkulatora w podejściu TDD z wykorzystaniem JUnit i generowaniem raportu");
     }
+
+    public double addition(double num1, double num2) {
+        return 0;
+    }
+
+    public double subtraction(double num1, double num2) {
+        return 0;
+    }
+
+    public double multiplication(double num1, double num2) {
+        return 0;
+    }
+
+    public double division(double num1, double num2) {
+        return 0;
+    }
 }
---
+++ src/test/java/pl/dominikgubala/advancedprogramming2/calculator/CalculatorTest.java
@@ -0,0 +1,32 @@
+package pl.dominikgubala.advancedprogramming2.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void additionTest() {
+        double result = calculator.addition(1, 2);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void subtractionTest() {
+        double result = calculator.subtraction(5, 4);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void multiplicationTest() {
+        double result = calculator.multiplication(9, 9);
+        assertEquals(81, result);
+    }
+
+    @Test
+    public void divisionTest() {
+        double result = calculator.division(42, 7);
+        assertEquals(7, result);
+    }
+}

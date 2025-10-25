d7e386dd74652deed84254023af8400f90d78bf7
dominikgubala <dominik.gubala@gmail.com>
2025-10-25 17:10:08 +0200

    add main class

---
+++ src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package pl.dominikgubala.advancedprogramming2.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Zad 1 - Tworzenie kalkulatora w podejściu TDD z wykorzystaniem JUnit i generowaniem raportu");
+    }
+}

88b58e2aba484f9ae509ae2386a5c31f947e0eb5
dominikgubala <dominik.gubala@gmail.com>
2025-10-25 17:44:29 +0200

    refactoring

--- src/test/java/pl/dominikgubala/advancedprogramming2/calculator/CalculatorTest.java
+++ src/test/java/pl/dominikgubala/advancedprogramming2/calculator/CalculatorTest.java
@@ -1,32 +1,34 @@
 package pl.dominikgubala.advancedprogramming2.calculator;
 
 import org.junit.jupiter.api.Test;
+import pl.dominikgubala.advancedprogramming2.calculator.model.CalculatorModel;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
-    private final Calculator calculator = new Calculator();
+    private final CalculatorModel calculator = new CalculatorModel();
 
     @Test
     public void additionTest() {
-        double result = calculator.addition(1, 2);
+        double result = calculator.add(1, 2);
         assertEquals(3, result);
     }
 
     @Test
     public void subtractionTest() {
-        double result = calculator.subtraction(5, 4);
+        double result = calculator.subtract(5, 4);
         assertEquals(1, result);
     }
 
     @Test
     public void multiplicationTest() {
-        double result = calculator.multiplication(9, 9);
+        double result = calculator.multiply(9, 9);
         assertEquals(81, result);
     }
 
     @Test
     public void divisionTest() {
-        double result = calculator.division(42, 7);
+        double result = calculator.divide(42, 7);
         assertEquals(6, result);
     }
 }

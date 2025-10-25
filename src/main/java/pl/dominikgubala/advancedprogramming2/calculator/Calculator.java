e11c7cc9508043fb82ad3ab68f7690a1224ddc6e
dominikgubala <dominik.gubala@gmail.com>
2025-10-25 17:31:45 +0200

    multiplication

--- src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
+++ src/main/java/pl/dominikgubala/advancedprogramming2/calculator/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
     }
 
     public double multiplication(double num1, double num2) {
-        return 0;
+        return num1 * num2;
     }
 
     public double division(double num1, double num2) {
